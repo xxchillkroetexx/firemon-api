4421b8dab8b0581f7fd89b197ed0ac856141baa8
Luke Johannsen <luke.johannsen@firemon.com>
2021-12-01 16:14:42 -0600

    moved configuration

--- setup.py
+++ setup.py
@@ -1,30 +1,5 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+import setuptools
 
-# Learn more: https://confluence.securepassage.com/display/DEVNETSEC/FMAPI%3A+Python+Firemon+API+module
-
-from setuptools import setup, find_packages
-from distutils.util import convert_path
-
-PROJECT = "firemon-api"
-
-setup(
-    name=PROJECT,
-    use_scm_version=True,
-    setup_requires=["setuptools_scm"],
-    description="NetSec Python Wrapper for Firemon API",
-    author="Firemon NetSec <dev-netsec@firemon.com>",
-    author_email="dev-netsec@firemon.com",
-    url="https://www.firemon.com/",
-    project_urls={
-        "Repository": "https://stash.securepassage.com/scm/nsu/firemon-api",
-        "Documentation": "https://confluence.securepassage.com/display/DEVNETSEC/FMAPI%3A+Python+Firemon+API+module",
-    },
-    packages=find_packages(exclude=("tests", "docs")),
-    include_package_data=True,
-    python_requires=">=3.8",
-    install_requires=[
-        "requests",
-        "setuptools_scm>=6.2",
-    ],
-    zip_safe=False,
-)
+if __name__ == "__main__":
+    setuptools.setup()
