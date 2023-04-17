c005e6c05983bd0afc49bad3cfe8b6d148756a9d
Luke Johannsen <luke.johannsne@firemon.com>
2023-04-17 16:33:22 -0500

    docs: remove github fork banner

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ html_static_path = ["_static"]
 html_logo = "logo_firemon.png"
 html_theme_options = {
     "show_powered_by": False,
-    "github_banner": True,
+    "github_banner": False,
     "show_related": False,
     "note_bg": "#FFF59C",
 }
