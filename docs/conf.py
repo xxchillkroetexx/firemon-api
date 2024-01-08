dad9a58ceebaa63acace9d3ab0138d92ed9a12bc
Luke Johannsen <luke.johannsne@firemon.com>
2024-01-08 16:44:59 -0600

    docs: readthedocs updates trying to fix webhook build

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 import pathlib
 import sys
 
-sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
+sys.path.insert(0, pathlib.Path(__file__).parents[1].resolve().as_posix())
 
 
 # -- Project information -----------------------------------------------------
